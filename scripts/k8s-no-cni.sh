38ee9fb7e85769c9eb808e73205e037f918c4b7e
saifeddine Rajhi <rajhiseif@gmail.com>
2024-06-23 13:08:19 +0200

    add k8s-no-cni.sh :rocket:

---
+++ scripts/k8s-no-cni.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Enable IPv4 forwarding
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# Script to install a single node K8s cluster on an k8s networking Lab
+#
+#
+# N.B. The script turns off swap for the K8s control plane install but does
+#      not disable swap permanently. Please comment out any swap volumes in
+#      the /etc/fstab before rebooting the VM.
+#
+#      This script will fail to run if the apt db is locked (wait 10 mins
+#      and retry or reboot and retry).
+#
+#      Kubernetes single node clusters require a 4GB ram VM to run properly.
+#
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null
+then
+    echo "Docker not found. Installing Docker..."
+    sudo apt-get update
+    wget -qO- https://get.docker.com/ | sh
+    sudo mkdir -p /etc/docker
+    cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+    sudo systemctl restart docker
+else
+    echo "Docker is already installed. Skipping Docker installation."
+fi
+
+# Configure Docker's bundled containerd to enable cni & use systemd for cgroups
+sudo cp /etc/containerd/config.toml /etc/containerd/config.bak
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i -e 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+# Initialize a control plane node
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo apt-get update
+sudo apt-get install -y kubeadm
+sudo swapoff -a
+K8S_VERSION=1.30.0
+if [ -z ${K8S_VERSION+x} ]; then K8S_VERSION="--kubernetes-version=stable-1" ; else K8S_VERSION="--kubernetes-version=$K8S_VERSION"; fi
+sudo kubeadm init --ignore-preflight-errors=FileExisting-conntrack --cri-socket=unix:///var/run/containerd/containerd.sock $K8S_VERSION  --pod-network-cidr=10.128.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl patch node $(hostname) -p '{"spec":{"taints":[]}}'
+
+# Install the latest crictl (cni-tools package is not always the latest)
+CRICTL_VERSION=$(curl -s https://api.github.com/repos/kubernetes-sigs/cri-tools/releases/latest|grep tag_name | cut -d '"' -f 4 | cut -b 2-)
+wget https://github.com/kubernetes-sigs/cri-tools/releases/download/v$CRICTL_VERSION/crictl-v$CRICTL_VERSION-linux-amd64.tar.gz
+sudo tar zxvf crictl-v$CRICTL_VERSION-linux-amd64.tar.gz -C /usr/local/bin
+rm -f crictl-v$CRICTL_VERSION-linux-amd64.tar.gz
+echo "runtime-endpoint: unix:///run/containerd/containerd.sock" | sudo tee /etc/crictl.yaml
